5e828cf330f311aeadd12e3d6b7388df10a4ea1d
tipe56 <s.m.ramaniuk@gmail.com>
2022-10-25 11:41:16 +0300

    add logo & version label update function

--- Tabata & HIIT Timer/ViewController.swift
+++ Tabata & HIIT Timer/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var labelVersion: UILabel!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        labelVersion.text = getVersion()
         // Do any additional setup after loading the view.
     }
 
-
 }
 
+
+func getVersion() -> String {
+    let dictionary      = Bundle.main.infoDictionary!
+    let appVersion      = dictionary["CFBundleShortVersionString"] as! String
+    let buildVersion    = dictionary["CFBundleVersion"] as! String
+
+    return "version \(appVersion).\(buildVersion)"
+}
